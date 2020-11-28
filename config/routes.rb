ead187e482a8e6f201e276c63fa92b6d8562ace2
ShichidaMasatsugu <shicchi.7913@icloud.com>
2020-11-28 12:09:28 +0900

    完成まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
   root to: 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
+  resources :events
+  resources :retirements
+  resources :events do
+    resources :tickets
+  end
 end
